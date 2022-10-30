991571e3dd651c32c5ee4ca27f10f2db18ab376c
nagata-ryosuke <114294761+nagata-ryosuke@users.noreply.github.com>
2022-10-30 18:27:26 +0900

    3章　チェックテスト
    3章　チェックテスト

--- PHPjob/3-3/index.php
+++ PHPjob/3-3/index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!-- PHP 3章 練習問題２ -->
 
 
@@ -19,3 +20,34 @@ step3:今日の日付と結果を表示する。 -->
     <input type="text" name="numbers" />
     <input type="submit" value="占う">
 </form>
+=======
+<!-- 3章 練習問題2 -->
+
+
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice2</title>
+</head>
+<body>
+
+<form action="result.php" method="post">
+    <p>好きな名前を入れてください。</p>
+    <input type="text" name="name" />
+    <br>
+    <p>1~6の中で好きな数字を入れてください。</p>
+    <input type="text" name="numbers" />
+    <br>
+    <br>
+    <input type="submit" value="送信">
+</form>
+
+  <!-- ①フォームを作成しましょう -->
+  <!-- ここにフォームを記述してください -->
+  </form>
+</body>
+</html>
+>>>>>>> Stashed changes
--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!-- 占い結果 -->
 
 <?php
@@ -13,3 +14,33 @@ $numbers = $_POST['numbers'];
     $result = rand(0,2);
     echo $omikuji[$result]; ?>
     </p>
+=======
+<!-- 3章 練習問題2 -->
+
+
+<?php
+$name= $_POST['name'];
+$numbers = $_POST['numbers'];
+?>
+
+
+<?php $number = $numbers * mt_rand(1,6);
+    if ($number >= 1 && $number <= 10) {
+        $result = '凶';
+        } else if ($number >= 11 && $number <= 15) {
+        $result = '小吉';
+        } else if ($number >= 16 && $number <= 20) {
+        $result = '中吉';
+        } else if ($number >= 21 && $number <= 25) {
+        $result = '吉';
+        } else if ($number >= 26 && $number <= 36) {
+        $result = '大吉';
+        } else if ($number >= 27)
+        { $result = '残念';
+        } ?>
+        
+    <p><?php echo date("Y/m/d H:i:s", time()); ?>の運勢は</p>
+    <p>名前は <?php echo $name; ?>  です。</p>
+    <p>番号は<?php echo $number ?>です。</p>
+    <p>結果は <?php echo $result ?>です。</p>
+>>>>>>> Stashed changes
