856b82e034d2414a6449c581eb85da3ab192cfcb
nagata-ryosuke <114294761+nagata-ryosuke@users.noreply.github.com>
2022-11-01 22:15:29 +0900

    3章　練習問題1
    3章　練習問題1

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -3,6 +3,7 @@
 
 <?php
 
+<<<<<<< Updated upstream
 const TAX = 1.1;
 
 $products = ["鉛筆" => 100 , "消しゴム" => 150 , "物差し" => 500];
@@ -11,6 +12,28 @@ foreach ($products as $key => $value) {
     echo $key;
     echo 'は';
     echo $value * TAX;
+=======
+$price1 = 100;
+$price2 = 150;
+$price3 = 500;
+
+function calc_tax($price)
+{
+  $tax = $price * 1.1;
+  return $tax;
+}
+
+$tax1 = calc_tax($price1);
+$tax2 = calc_tax($price2);
+$tax3 = calc_tax($price3);
+
+$products = ["鉛筆" => $tax1 , "消しゴム" => $tax2 , "物差し" => $tax3];
+
+foreach ($products as $key => $value) {
+    echo $key;
+    echo 'の税込価格は';
+    echo $value;
+>>>>>>> Stashed changes
     echo '円です。';
     echo '<br>';
 }
